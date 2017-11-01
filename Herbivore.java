591967d7f183213952ccb59eb532ddb94c7fa6f8
Jacky Li <jinglun.li@alumni.ubc.ca>
2017-10-31 19:34:18 -0700

    ITS DONE, SAY NO TO CHECKSTYLE

--- Herbivore.java
+++ Herbivore.java
@@ -2,13 +2,24 @@ package ca.bcit.comp2526.a2a;
 
 import java.util.ArrayList;
 
+/**
+ * Creates an Entity of type Herbivore.
+ * @author Jacky
+ * @version 1.0a
+ */
 public class Herbivore extends Entity {
     private static ArrayList<Herbivore> allH = new ArrayList<Herbivore>();
     private static int totalNum = allH.size();
-    private final int MAXHP = 10;
+    private static final int MAXHP = 10;
+    private static final int DECREMENT = 1;
     private int hitPoint;
     private boolean moved;
 
+
+    /**
+     * Creates a Herbivore object.
+     * @param cell  the cell which this Herbivore is created in
+     */
     public Herbivore(Cell cell) {
         super(cell);
     }
@@ -25,45 +36,64 @@ public class Herbivore extends Entity {
     
     /**
      * Removes a given cell's reference to a Plant object. resets hitPoint.
-     * @param plant
+     * @param cell the cell to remove the Plant object from.
      */
     public void eatPlant(Cell cell) {
-        if(cell.getPlant()!= null) {
+        if (cell.getPlant() != null) {
             cell.removePlant();
             resetHp();
         }
     }
 
+    /**
+     * Decrements the HitPoint of this Herbivore by 1.
+     */
     public void minusHitPoint() {
-        this.hitPoint--;
+        this.hitPoint -= DECREMENT;
     }
 
     /**
-     * Resets the current Herbivore HP to MAXHP
+     * Resets the current Herbivore HP to MAXHP.
      */
     public void resetHp() {
         this.hitPoint = MAXHP;
     }
 
+    /**
+     * Grabs all alive Herbivores.
+     * @return an array of Herbivores
+     */
     public static Herbivore[] getAllHerbivores() {
         return allH.toArray(new Herbivore[allH.size()]);
     }
 
+    /**
+     * Removes this herbivore from index of herbivores.
+     */
     public void removeFromAllHerbs() {
         allH.remove(this);
+        totalNum--;
     }
     
+    /**
+     * Returns true if herbivore already moved this turn.
+     * @return true if this Herbivore already moved
+     */
     public boolean haveMoved() {
         return this.moved;
     }
     
+    /**
+     * Sets the herbivore's moved status.
+     * @param b return true if Herbivore already moved
+     */
     public void setMoved(boolean b) {
         this.moved = b;
     }
     
     /**
      * Checks to see if this herbivore is dead.
-     * @return
+     * @return true if dead, false if alive
      */
     public boolean isDead() {
         if (this.hitPoint <= 0) {
@@ -74,6 +104,7 @@ public class Herbivore extends Entity {
     
     /**
      * Moves the herbivore, returns the new Cell the herbivore moved to.
+     * @return the Cell the herbivore is moving into
      */
     public Cell moveHerbivore() {
         // Grabbing adj cells, put in
@@ -107,14 +138,16 @@ public class Herbivore extends Entity {
 
     /**
      * Selects a Cell of the prioritized EntityType, if exists.
-     * @param cells
-     * @param et
-     * @param size
-     * @return
+     * @param cells given array of cells to select from
+     * @param et    EntityType to prioritize
+     * @param size  the given cells array size
+     * @return the randomly selected Cell to be prioritized to
      */
     private Cell getPriorityCell(Cell[] cells, EntityType et, int size) {
-        int pInd = 0, otherInd = 0;
-        Cell[] pCells = new Cell[size], otherCells = new Cell[size];
+        int pInd = 0;
+        int otherInd = 0;
+        Cell[] pCells = new Cell[size];
+        Cell[] otherCells = new Cell[size];
         for (Cell c : cells) {
             if (c == null) {
                 break;
@@ -130,8 +163,8 @@ public class Herbivore extends Entity {
     }
 
     /**
-     * Selects a Cell from 2 lists: priority, and non-priority. Selects priority
-     * Cells if array not null
+     * Selects a Cell from 2 lists: priority, and non-priority. 
+     * Selects from priority cell array if not empty.
      * 
      * @param pCells
      *            array of prioritized Cells
@@ -141,9 +174,10 @@ public class Herbivore extends Entity {
      *            array of non-priority Cells
      * @param oInd
      *            number of elements in non-p Cells
-     * @return
-     */
-    private Cell selectPriority(Cell[] pCells, int pInd, Cell[] otherCells, int oInd) {
+     * @return the Cell to be returned.
+     */ 
+    private Cell selectPriority(Cell[] pCells, int pInd, 
+            Cell[] otherCells, int oInd) {
         Cell priority = null;
         if (pInd > 0) {
             priority = pCells[RandomGenerator.nextNumber(pInd)];
@@ -153,5 +187,11 @@ public class Herbivore extends Entity {
         return priority;
     }
     
-
+    /**
+     * Returns the total amount of alive herbivores.
+     * @return total number of alive Herbivores.
+     */
+    public static int countTotalAlive() {
+        return totalNum;
+    }
 }
